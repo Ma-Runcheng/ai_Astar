b747768b4d1e14aa02ab41447cb38b1608ba4196
MaRuncehng <MaRuncheng0@gmail.com>
2021-12-08 19:54:41 +0800

    init

--- src/com/BindMapExample/ButtonPanel.java
+++ src/com/BindMapExample/ButtonPanel.java
@@ -22,60 +22,21 @@ public class ButtonPanel extends JPanel implements Observer{
         add(fileBtn);
 
         roadBtn.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                Notify(mapPanel,"empty");
-            }
+
         });
 
         obstacleBtn.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                Notify(mapPanel,"obstacle");
-            }
+
         });
 
         fileBtn.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                if(check()){
-                    File mat = new File("mat.txt");
-                    BufferedWriter bw;
-                    try {
-                        mat.createNewFile();
-                        bw = new BufferedWriter(new FileWriter(mat));
-                        for (int i = 0; i < map[0].length; i++) {
-                            for (int j = 0; j < map.length; j++) {
-                                bw.write(map[j][i]+"");
-                                if(j != map[0].length - 1) bw.write(",");
-                            }
-                            bw.write("\n");
-                        }
-                        bw.close();
-                        JOptionPane.showMessageDialog(null,"生成成功");
-                    } catch (IOException ioException) {
-                        ioException.printStackTrace();
-                    }
-                }else{
-                    JOptionPane.showMessageDialog(null,"没选完");
-                }
-            }
+
         });
     }
 
-    private boolean check() {
-        for (int i = 0; i < map.length; i++) {
-            for (int j = 0; j < map[0].length; j++) {
-                if(map[i][j] == 0){
-                    return true;
-                }
-            }
-        }
-        return true;
-    }
 
     @Override
     public void Notify(Observable o,String status) {
-        o.update(status);
+
     }
 }
--- src/com/BindMapExample/mapPanel.java
+++ src/com/BindMapExample/mapPanel.java
@@ -14,53 +14,21 @@ class mapPanel extends JPanel implements Observable{
     int status = -1;
     public mapPanel() {
         this.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                int x = round(e.getX());
-                int y = round(e.getY());
-                System.out.println(x+ " " + y);
-                mapGrid[x][y] = status;
-                repaint();
-            }
+
         });
     }
 
-    private int round(int x){
-        return x / 20;
-    }
 
 
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.drawImage(mapImg,0,0,mapImg.getWidth(null),mapImg.getHeight(null),null);
-        for (int i = 0; i < wNum; i++) {
-            for (int j = 0; j < hNum; j++) {
-                if(mapGrid[i][j] == 1){
-                    g.setColor(Color.RED);
-                    g.fillRect(i*20,j*20,5,5);
-                }else if(mapGrid[i][j] == -1){
-                    g.setColor(Color.GREEN);
-                    g.fillRect(i*20,j*20,5,5);
-                }else{
-                    g.setColor(Color.BLACK);
-                    g.drawRect(i*20,j*20,5,5);
-                }
-            }
-        }
+
     }
 
     @Override
     public void update(String status) {
-        if("empty".equals(status)){
-            getGraphics().setColor(Color.GREEN);
-            this.status = -1;
-        }else if("obstacle".equals(status)){
-            getGraphics().setColor(Color.RED);
-            this.status = 1;
-        }else if("file".equals(status)){
 
-        }
     }
 }
